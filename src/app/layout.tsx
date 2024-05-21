c0a9a25e60cbb3e6ad4e73ad75078da7a9451fb3
benfogiel <ben.fogiel@saberastro.com>
2024-05-20 19:31:29 -0700

    added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import "../index.css";
-
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -14,6 +13,39 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+            <head>
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-touch-icon.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/x-icon"
+                    sizes="64x64 32x32 24x24 16x16"
+                    href="/favicon.ico"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="192x192"
+                    href="/logo192.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="512x512"
+                    href="/logo512.png"
+                />
+                <link rel="manifest" href="/manifest.json" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta
+                    name="apple-mobile-web-app-status-bar-style"
+                    content="black"
+                />
+                <meta name="apple-mobile-web-app-title" content="Pollinator" />
+                <meta name="theme-color" content="#000000" />
+            </head>
             <body>
                 <div id="root">{children}</div>
             </body>

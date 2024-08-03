782593db3ad39fab5b780d7c9b9ca011efc1b099
benfogiel <benjaminwfogiel@gmail.com>
2024-08-02 20:28:16 -0700

    added capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+    appId: "pollinator.app",
+    appName: "pollinator",
+    webDir: "dist",
+};
+
+export default config;

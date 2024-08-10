3deb7183c8af5f8cf0309814e866d415c0e71af8
benfogiel <benjaminwfogiel@gmail.com>
2024-08-10 00:25:52 -0700

    added circuitpython code

---
+++ circuit_python/helpers/__init__.py
@@ -0,0 +1,2 @@
+from .flower import Flower
+from .util import load_env_file
---
+++ circuit_python/helpers/flower.py
@@ -0,0 +1,72 @@
+import neopixel
+from .util import parse_hex_color, hsv_to_rgb
+
+
+class Flower:
+
+    def __init__(self, num_leds, pedal_length, data_pin):
+        self.num_leds = num_leds
+        self.pedal_length = pedal_length
+        self.data_pin = data_pin
+        self.current_motion_state = "idle"
+        self.update_rate = 1.0  # in seconds
+        self.leds = neopixel.NeoPixel(self.data_pin, num_leds, brightness=0.8, auto_write=False)
+
+        self.MOTION_STATES = {"idle": lambda: None, "swirl": self.swirl}
+
+    def set_current_motion_state(self, state):
+        if state not in self.MOTION_STATES:
+            raise ValueError(f"State does not exist: {state}")
+        self.current_motion_state = state
+
+    def set_brightness(self, brightness):
+        self.leds.brightness = brightness
+        self.leds.show()
+    
+    def set_update_rate(self, rate):
+        self.update_rate = rate
+
+    def update(self):
+        if self.current_motion_state in self.MOTION_STATES:
+            self.MOTION_STATES[self.current_motion_state]()
+    
+    def get_pedal_leds(self, pedal_index):
+        return range(
+            pedal_index * self.pedal_length,
+            min((pedal_index + 1) * self.pedal_length, self.num_leds),
+        )
+
+    def set_pedal_leds(self, pedal_index, color):
+        for i in self.get_pedal_leds(pedal_index):
+            self.leds[i] = color
+        self.leds.show()
+
+    def set_color_all(self, hex_color):
+        color = parse_hex_color(hex_color)
+        self.leds.fill(color)
+        self.leds.show()
+
+    def set_gradient(self, hex_color1, hex_color2):
+        color1 = parse_hex_color(hex_color1)
+        color2 = parse_hex_color(hex_color2)
+        for i in range(self.num_leds):
+            ratio = i / self.num_leds
+            color = tuple(
+                int(color1[j] + ratio * (color2[j] - color1[j])) for j in range(3)
+            )
+            self.leds[i] = color
+        self.leds.show()
+
+    def rainbow(self):
+        for i in range(self.num_leds):
+            hue = i / self.num_leds
+            rgb = hsv_to_rgb(hue, 1.0, 1.0)
+            self.leds[i] = rgb
+        self.leds.show()
+
+    def swirl(self):
+        temp = self.leds[-1]
+        for i in range(self.num_leds - 1, 0, -1):
+            self.leds[i] = self.leds[i - 1]
+        self.leds[0] = temp
+        self.leds.show()
